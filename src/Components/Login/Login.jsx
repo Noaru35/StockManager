d7c7e3153e86893e383bfcbad2c9b3e5c9f37662
noaru35 <vinicius.s.bastos55@gmail.com>
2024-09-11 22:34:10 -0300

    Criando repositorio

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,50 @@
+import {FaUser, FaLock} from 'react-icons/fa'
+
+import { useState } from 'react'
+
+import "./Login.css";
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        alert("Enviando os dados! " + username + " - " + password);
+    }
+
+  return (
+    <div className="container">
+        <form onSubmit={handleSubmit}>
+            <h1>Acesse o Sistema</h1>
+            <div className="input-field">
+                <input type="email" placeholder="Email" onChange =
+                {(e) => setUsername(e.target.value)}/>
+                <FaUser className="icon" />  
+            </div>
+            <div className="input-field">
+                <input type="password" placeholder="Senha" onChange = 
+                {(e) => setPassword(e.target.value)}/>
+                <FaLock className="icon" />
+            </div>
+            
+            <div className="recall-forget">
+                <label htmlFor="">
+                    <input type="checkbox"/>
+                    Lembre de mim
+                </label>
+                <a href="#">Esqueceu a Senha?</a>
+            </div>
+            
+            <button>Entrar</button>
+
+            <div className="signup-link">
+                <p>Não tem uma conta? <a href="#">Registre-se!</a></p>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
